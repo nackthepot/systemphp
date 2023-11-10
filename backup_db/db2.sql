989470c7df21434dc39e481b57a2822187f508c0
nackthepot <139296599+nackthepot@users.noreply.github.com>
2023-11-11 00:11:53 +0700

    update database

--- backup_db/db2.sql
+++ backup_db/db2.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 08, 2023 at 05:31 PM
+-- Generation Time: Nov 10, 2023 at 06:11 PM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.0.28
 
@@ -24,6 +24,37 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `category`
+--
+
+CREATE TABLE `category` (
+  `category_id` int(11) NOT NULL,
+  `category_name` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `category`
+--
+
+INSERT INTO `category` (`category_id`, `category_name`) VALUES
+(1, 'mouse'),
+(2, 'keyboard'),
+(3, 'headset');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `group`
+--
+
+CREATE TABLE `group` (
+  `group_id` int(11) NOT NULL,
+  `group_name` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `price`
 --
 
@@ -40,9 +71,47 @@ CREATE TABLE `price` (
 
 INSERT INTO `price` (`sku`, `price_buy`, `price_sell`, `price_net`) VALUES
 ('NB01', 10000, 12000, 11500),
-('NB03', 0, 20000, 19500),
-('NB04', 20000, 21000, 20500),
-('NB02', 15000, 17000, 16300);
+('NB03', 0, 28000, 27000),
+('NB04', 20000, 19500, 19000),
+('NB02', 15000, 17000, 16300),
+('555', 10, 0, 0),
+('556', 2400, 0, 0),
+('NB005', 9600, 15000, 14700),
+('NB03', 25000, 28000, 27000),
+('MU01', 120, 290, 250);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `products`
+--
+
+CREATE TABLE `products` (
+  `id` int(11) NOT NULL,
+  `sku` varchar(255) NOT NULL,
+  `name_pd` varchar(255) NOT NULL,
+  `category_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `products`
+--
+
+INSERT INTO `products` (`id`, `sku`, `name_pd`, `category_id`) VALUES
+(1, 'MU01', 'OKER GAMING', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `product_all`
+--
+
+CREATE TABLE `product_all` (
+  `id` int(11) NOT NULL,
+  `sku` varchar(255) NOT NULL,
+  `name_pd` varchar(255) NOT NULL,
+  `group_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -62,14 +131,26 @@ CREATE TABLE `product_nb` (
 --
 
 INSERT INTO `product_nb` (`id`, `sku`, `name_nb`, `stock_id`) VALUES
-(1, 'NB01', 'MSI GF66 Thin I5 gen12', 0),
 (2, 'NB02', 'ASUS TUF F15 RTX3060', 0),
-(3, 'NB03', 'LENOVO IDEAPAD', 0),
-(4, 'NB04', 'ASUS TUF A18', 0);
+(4, 'NB04', 'ASUS TUF A18', 0),
+(9, 'NB01', 'lenovo yoga', 0),
+(15, 'NB03', 'MSI GAMING I7', 0);
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `products`
+--
+ALTER TABLE `products`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `product_all`
+--
+ALTER TABLE `product_all`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `product_nb`
@@ -82,10 +163,22 @@ ALTER TABLE `product_nb`
 --
 
 --
+-- AUTO_INCREMENT for table `products`
+--
+ALTER TABLE `products`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `product_all`
+--
+ALTER TABLE `product_all`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT for table `product_nb`
 --
 ALTER TABLE `product_nb`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
