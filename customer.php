718448e1b28ac5ce11eaf77a72ce1837c2665ffc
nackthepot <139296599+nackthepot@users.noreply.github.com>
2023-11-21 22:59:30 +0700

    เริ่มทำหน้าเปิดบิล

---
+++ customer.php
@@ -0,0 +1,93 @@
+<?php
+    include("php/connectdb.php")
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+table {
+  border: 1px solid black;
+}
+th {
+  color: white;
+  padding: 20px;
+  background-color: blue;
+  border: 1px solid black;
+}
+td {
+  width: 300px;
+  border: 1px solid black;
+  height: 40px;
+}
+tr {
+    
+}
+
+.btn{
+    background-color: red;
+    padding: 2px 5px;
+    border-radius: 5px;
+    text-decoration: none;
+    color : white;
+}
+
+.btn1{
+    background-color: green;
+    margin-left: 10px;
+}
+
+.btn2{
+    background-color: red;
+}
+</style>
+<link rel="stylesheet" href="style.css">
+</head>
+<body>
+<?php include("php/menu.php") ?><br>
+    <h1>หน้าหลัก</h1>
+    
+    <div class="wrapper">
+<table class="table">
+    <thead>
+        <td>รายการ Notebook</td>
+        <tr>
+            <th>รหัสลูกค้า</th>
+            <th>ชื่อ</th>
+            <th>ที่อยู่</th>
+            <th>เบอร์ติดต่อ/th>
+            <th>ACTION</th>
+        </tr>
+    </thead>
+    
+        <?php
+        $sql = "SELECT *
+                FROM customer
+                ";
+        $result = mysqli_query($conn,$sql);
+
+        while($row = $result->fetch_assoc()){
+            echo "
+            <tr>
+       
+            <td>$row[customer_id]</td>
+            <td>$row[name]</td>
+            <td>$row[address]</td>
+            <td>$row[phone]</td>
+            <td>
+                <a class='btn btn1' href='new.php?customer_id=$row[customer_id]&name=$row[name]'>เลือก</a>
+
+            </td>
+        </tr>
+        ";
+        }
+    
+
+    ?>
+    
+
+    
+</body>
+</html>
