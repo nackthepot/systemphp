7d04e84f7f33d04840df82db65835cc27d5e0de2
nackthepot <139296599+nackthepot@users.noreply.github.com>
2023-11-24 08:31:12 +0700

    ทำ db ใหม่ให้ครบ

--- backup_db/pkj_db.sql
+++ backup_db/pkj_db.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.1
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Nov 24, 2023 at 01:05 AM
--- Server version: 10.4.32-MariaDB
--- PHP Version: 8.0.30
+-- Host: localhost:8889
+-- Generation Time: Nov 24, 2023 at 01:29 AM
+-- Server version: 5.7.39
+-- PHP Version: 8.2.0
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,6 +24,17 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `category`
+--
+
+CREATE TABLE `category` (
+  `category_id` int(11) NOT NULL,
+  `category_name` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `customers`
 --
 
@@ -33,7 +44,7 @@ CREATE TABLE `customers` (
   `customer_address` varchar(255) NOT NULL,
   `customer_town` varchar(255) NOT NULL,
   `customer_phone` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `customers`
@@ -43,9 +54,134 @@ INSERT INTO `customers` (`customer_id`, `customer_name`, `customer_address`, `cu
 (1, 'นายแดง', '199/1', 'กรุงเทพ', '0661116666'),
 (2, 'นายดำ', '191 หมู่ 5', 'เชียงใหม่', '0989989998');
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `dealer`
+--
+
+CREATE TABLE `dealer` (
+  `dealer_id` int(11) NOT NULL,
+  `dealer_name` varchar(255) NOT NULL,
+  `dealer_address` varchar(255) NOT NULL,
+  `dealer_phone` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `employee`
+--
+
+CREATE TABLE `employee` (
+  `employee_id` int(11) NOT NULL,
+  `employee_name` varchar(255) NOT NULL,
+  `employee_phone` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `products`
+--
+
+CREATE TABLE `products` (
+  `id` int(11) NOT NULL,
+  `sku` varchar(255) NOT NULL,
+  `category_id` int(11) NOT NULL,
+  `brand` varchar(255) NOT NULL,
+  `model` varchar(255) NOT NULL,
+  `serial` varchar(255) NOT NULL,
+  `date` date NOT NULL,
+  `warranty_com_id` int(11) NOT NULL,
+  `warraty` date NOT NULL,
+  `dealer_id` int(11) NOT NULL,
+  `price_buy` decimal(10,2) NOT NULL,
+  `price_sell` decimal(10,2) NOT NULL,
+  `price_net` decimal(10,2) NOT NULL,
+  `comment` varchar(255) NOT NULL,
+  `employee_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `spec_monitor`
+--
+
+CREATE TABLE `spec_monitor` (
+  `sku` varchar(255) NOT NULL,
+  `size` varchar(255) NOT NULL,
+  `resolution` varchar(255) NOT NULL,
+  `panel` varchar(255) NOT NULL,
+  `hz` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `spec_nb`
+--
+
+CREATE TABLE `spec_nb` (
+  `sku` varchar(255) NOT NULL,
+  `cpu` varchar(255) NOT NULL,
+  `ram` varchar(255) NOT NULL,
+  `ssd1` varchar(255) NOT NULL,
+  `ssd2` varchar(255) NOT NULL,
+  `hdd` varchar(255) NOT NULL,
+  `vga` varchar(255) NOT NULL,
+  `display` varchar(255) NOT NULL,
+  `adapter` varchar(255) NOT NULL,
+  `acc` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `stock`
+--
+
+CREATE TABLE `stock` (
+  `sku` varchar(255) NOT NULL,
+  `quantity` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `stockplace`
+--
+
+CREATE TABLE `stockplace` (
+  `sku` varchar(255) NOT NULL,
+  `inshop` int(11) NOT NULL,
+  `box1` int(11) NOT NULL,
+  `f3` int(11) NOT NULL,
+  `f4` int(11) NOT NULL,
+  `comment` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `warranty_com`
+--
+
+CREATE TABLE `warranty_com` (
+  `warranty_com_id` int(11) NOT NULL,
+  `warranty_com_name` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `category`
+--
+ALTER TABLE `category`
+  ADD PRIMARY KEY (`category_id`);
 
 --
 -- Indexes for table `customers`
@@ -54,14 +190,32 @@ ALTER TABLE `customers`
   ADD PRIMARY KEY (`customer_id`);
 
 --
+-- Indexes for table `warranty_com`
+--
+ALTER TABLE `warranty_com`
+  ADD PRIMARY KEY (`warranty_com_id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
+
+--
+-- AUTO_INCREMENT for table `category`
+--
+ALTER TABLE `category`
+  MODIFY `category_id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `customers`
 --
 ALTER TABLE `customers`
   MODIFY `customer_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `warranty_com`
+--
+ALTER TABLE `warranty_com`
+  MODIFY `warranty_com_id` int(11) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
