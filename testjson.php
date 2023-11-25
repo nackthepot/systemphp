0d7e52106fea9d22937411fbea8b67fdb835c8de
nackthepot <139296599+nackthepot@users.noreply.github.com>
2023-11-25 09:33:30 +0700

    Create testjson.php

---
+++ testjson.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php include("php/connectdb.php")?>
+    <?php include("php/menu.php")?>
+    <br>
+<form action="" method="POST">
+    <input type="text" name="name">
+    <input type="text" name="cpu">
+    <input type="text" name="ram">
+    <input type="text" name="vga">
+    <input type="text" name="price">
+    <input type="submit" value="ok">
+</body>
+</html>
+    <?php
+        $name = $_POST["name"];
+        $cpu = $_POST["cpu"];
+        $ram = $_POST["ram"];
+        $vga = $_POST["vga"];
+        $price = $_POST["price"];
+        $spec = ["cpu" => $cpu,
+                 "ram" => $ram,
+                 "vga" => $vga];
+        $json = json_encode($spec);
+        $sql = "INSERT INTO products (name,spec,price)
+                VALUES ('$name','$json','$price')";
+        $result = mysqli_query($conn,$sql);
+        $stmt = $conn ->prepare($sql);
+        
+
+        echo $name . "<br>";
+        echo $json . "<br>";
+        echo $price ;
+
+    ?>
